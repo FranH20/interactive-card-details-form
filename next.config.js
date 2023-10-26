1841acee265fce303e56915bcf532819a8d9c11d
Franklin Huichi <carloshuichi@gmail.com>
2023-10-25 21:04:50 -0500

    Add images config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 const nextConfig = {
   output: "export",
   basePath: '/interactive-card-details-form',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true
-  }
+  },
 }
 
 module.exports = nextConfig
